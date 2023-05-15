c93babc79d9d651b35237abe1d25b3a6c477496a
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-15 21:36:12 +0200

    Commit 2. el programa pasa los test de manera general

--- SongTDD/src/TDDSong.java
+++ SongTDD/src/TDDSong.java
@@ -3,30 +3,43 @@ import java.util.ArrayList;
 public class TDDSong {
     private static String resultado="";
     private static String estrofa1 =
-            "There was an old lady who swallowed a fly.\n" +
-            "I don't know why she swallowed a fly - perhaps she'll die!\n";
+            "There was an old lady who swallowed a animal1.\n" +
+            "I don't know why she swallowed a animal1 - perhaps she'll die!\n";
 
     private static String estrofa2 =
-            "There was an old lady who swallowed a spider;\n" +
+            "\n" +
+            "There was an old lady who swallowed a animal2;\n" +
             "That wriggled and wiggled and tickled inside her.\n" +
-            "She swallowed the spider to catch the fly;\n" +
-            "I don't know why she swallowed a fly- perhaps she'll die!\n";
+            "She swallowed the animal2 to catch the animal1;\n" +
+            "I don't know why she swallowed a animal1 - perhaps she'll die!\n";
 
     private static String estrofa3 =
-            "There was an old lady who swallowed a bird;\n" +
-            "How absurd to swallow a bird.\n" +
-            "She swallowed the bird to catch the spider,\n" +
-            "She swallowed the spider to catch the fly;\n" +
-            "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+            "There was an old lady who swallowed a animal3;\n" +
+            "How absurd to swallow a animal3.\n" +
+            "She swallowed the animal3 to catch the animal2,\n" +
+            "She swallowed the animal2 to catch the animal1;\n" +
+            "I don't know why she swallowed a animal1 - perhaps she'll die!\n" +
             "\n";
 
     public static String filtroParrafos(ArrayList<String> animales) {
         if (animales.size() == 1) {
             resultado = estrofa1;
         } else if (animales.size() == 2) {
-            resultado = estrofa2;
+            resultado = estrofa1 + estrofa2;
         } else if (animales.size() == 3) {
-            resultado = estrofa3;
+            resultado = estrofa1 + estrofa2 +estrofa3;
+        }
+
+        if (animales.size() == 1) {
+            resultado=resultado.replaceAll("animal1", animales.get(0));
+
+        } else if (animales.size() == 2) {
+            resultado=resultado.replaceAll("animal1", animales.get(0));
+            resultado=resultado.replaceAll("animal2", animales.get(1));
+        } else if (animales.size() == 3) {
+            resultado=resultado.replaceAll("animal1", animales.get(0));
+            resultado=resultado.replaceAll("animal2", animales.get(1));
+            resultado=resultado.replaceAll("animal3", animales.get(2));
         }
         return resultado;
     }
--- SongTDD/src/TDDSongTest.java
+++ SongTDD/src/TDDSongTest.java
@@ -15,4 +15,65 @@ class TDDSongTest {
         assertEquals(resultadoEsperado, resultadoReal);
     }
 
+    @Test
+    public void test2(){
+        ArrayList<String> animal = new ArrayList<>();
+        animal.add("fly");
+        String resultadoEsperado= "There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" ;
+        String resultadoReal = TDDSong.filtroParrafos(animal) ;
+        System.out.println(resultadoReal);
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test3(){
+        ArrayList<String> animal = new ArrayList<>();
+        animal.add("spider");
+        String resultadoEsperado= "There was an old lady who swallowed a spider.\n" +
+                "I don't know why she swallowed a spider - perhaps she'll die!\n" ; ;
+        String resultadoReal = TDDSong.filtroParrafos(animal) ;
+        System.out.println(resultadoReal);
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test4(){
+        ArrayList<String> animal = new ArrayList<>();
+        animal.add("fly");
+        animal.add("spider");
+        String resultadoEsperado= "There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a spider;\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly;\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n";
+        String resultadoReal = TDDSong.filtroParrafos(animal) ;
+        System.out.println(resultadoReal);
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test5(){
+        ArrayList<String> animal = new ArrayList<>();
+        animal.add("fly");
+        animal.add("spider");
+        animal.add("bird");
+        String resultadoEsperado= "There was an old lady who swallowed a spider.\n" +
+                "I don't know why she swallowed a spider - perhaps she'll die!\n" +
+                "\n" +
+                "There was an old lady who swallowed a fly;\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the fly to catch the spider;\n" +
+                "I don't know why she swallowed a spider - perhaps she'll die!\n";
+        String resultadoReal = TDDSong.filtroParrafos(animal) ;
+        System.out.println(resultadoReal);
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
 }
