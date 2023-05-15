e441372ad04d633ad39860c3ed405b85fcd23ece
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-15 21:46:45 +0200

    Commit 5. no repetir if identicos

--- SongTDD/src/TDDSong.java
+++ SongTDD/src/TDDSong.java
@@ -25,19 +25,13 @@ public class TDDSong {
     public static String filtroParrafos(ArrayList<String> animales) {
         if (animales.size() == 1) {
             resultado = estrofa1;
+            resultado=resultado.replaceAll("animal1", animales.get(0));
         } else if (animales.size() == 2) {
             resultado = estrofa1 + estrofa2;
-        } else if (animales.size() == 3) {
-            resultado = estrofa1 + estrofa2 +estrofa3;
-        }
-
-        if (animales.size() == 1) {
-            resultado=resultado.replaceAll("animal1", animales.get(0));
-
-        } else if (animales.size() == 2) {
             resultado=resultado.replaceAll("animal1", animales.get(0));
             resultado=resultado.replaceAll("animal2", animales.get(1));
         } else if (animales.size() == 3) {
+            resultado = estrofa1 + estrofa2 +estrofa3;
             resultado=resultado.replaceAll("animal1", animales.get(0));
             resultado=resultado.replaceAll("animal2", animales.get(1));
             resultado=resultado.replaceAll("animal3", animales.get(2));
